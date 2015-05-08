2af0a480df5263b02792d3b040fa9275e46675a0
HongAnhVu <vuhonganh91@gmail.com>
2015-05-08 02:08:19 +0200

    clean code

--- self_study/mice_and_maze_uva_1112/mice_and_maze.cpp
+++ self_study/mice_and_maze_uva_1112/mice_and_maze.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define szAr 128
 #define MAX_VAL 1000000000
 
+bool debug = false;
 
 int N; //nb of cells (i.e. vertices)
 int E; //exit cell
@@ -27,53 +28,68 @@ void initGraph()
 
 int main()
 {
+  int checkRead;
   //read input
   int nbCases;
   
   int a, b, t; // a ->  b takes t unit of time
   
   //read data
-  scanf("%d", &nbCases);
-  
+  checkRead =  scanf("%d", &nbCases);
+  if(checkRead != 1 && debug)
+    cout << "sth wrong with read input nbCases\n";
   //for each test case
   while(nbCases)
     {
-      scanf("%d %d %d %d", &N, &E, &T, &M);
-      
-      //re initialize according to the new size array
-      //initGraph();
+      checkRead = scanf("%d %d %d %d", &N, &E, &T, &M);
+      if(checkRead != 4 && debug)
+	cout << "sth wrong with read input N, E, T, M\n";
 
+      //re initialize according to the new size array
+      initGraph();
+      /*
       for(int i = 0; i < N; ++i)
 	{
 	  for(int j = 0; j < N; ++j)
 	    adjMatrix[i][j] = MAX_VAL;
 	  
 	  adjMatrix[i][i] = 0;
-	}
+	  }*/
       //read edges' weight
       for(int j = 0; j < M; ++j)
 	{
-	  scanf("%d %d %d", &a, &b, &t);
+	  checkRead = scanf("%d %d %d", &a, &b, &t);
+	  if(checkRead != 3 && debug)
+	    cout << j << " =j, sth wrong with read input a, b, t\n";
 	  adjMatrix[a-1][b-1] = t;
-	  //cout << "connection " << j << ": " << a << b << t << endl;
-	  // cout << "adjMatrix[" << a+1 << "][" << b+1 << "]" << adjMatrix[a][b] << endl;
+	  if(debug)
+	    {
+	      cout << "connection " << j << ": " << a << b << t << endl;
+	      cout << "adjMatrix[" << a+1 << "][" << b+1 << "]" << adjMatrix[a][b] << endl;
+	    }
 	}
   
-      //cout << "\nafter read input\n";
-      /*for(int i = 0; i < N; i++)
-	for(int j = 0; j < N; j++)
-	cout << "adjMatrix[" << i+1 << "][" << j+1 << "]" << adjMatrix[i][j] << endl;*/
- 
+      if(debug)
+	{
+	  cout << "\nafter read input\n";
+	  for(int i = 0; i < N; i++)
+	    for(int j = 0; j < N; j++)
+	      cout << "adjMatrix[" << i+1 << "][" << j+1 << "]" << adjMatrix[i][j] << endl;	  
+	}
+      
       //floyd warshall
       for(int k = 0; k < N; ++k)
 	for(int i = 0; i < N; ++i)
 	  for(int j = 0; j < N; ++j)
 	    adjMatrix[i][j] = min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j]);
 
-      /*cout << "\n after floyd warshall\n";
-      for(int i = 0; i < N; i++)
-	for(int j = 0; j < N; j++)
-	cout << "adjMatrix[" << i+1 << "][" << j+1 << "]" << adjMatrix[i][j] << endl;*/
+      if(debug)
+	{
+	  cout << "\n after floyd warshall\n";
+	  for(int i = 0; i < N; i++)
+	    for(int j = 0; j < N; j++)
+	      cout << "adjMatrix[" << i+1 << "][" << j+1 << "]" << adjMatrix[i][j] << endl;	  
+	}
       
       int res = 0;
       for(int i = 0; i < N; ++i)
