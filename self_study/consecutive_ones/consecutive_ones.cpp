97392ed5614b226c477c0c295816089a2d75fbe9
HongAnhVU <vuhonganh91@gmail.com>
2015-05-18 02:46:29 +0200

    consecutive ones WIP

--- self_study/consecutive_ones/consecutive_ones.cpp
+++ self_study/consecutive_ones/consecutive_ones.cpp
@@ -32,10 +32,11 @@ int nbLines, nbCols;
 int matIn[SZ_AR][SZ_AR];//will read like this mat[idx_Col][idx_Row]
 int matOut[SZ_AR][SZ_AR];
 int idxLastOne[SZ_AR];//for each row, return the idx of last 1 added
-int idNextCol = 0;
+//int idxLastOnePrevious[SZ_AR];
+int idNextCol;
 
 set <int> Q;
-
+vi results;
 void readIn()
 {
   checkRead = scanf("%d", &nbLines); assert(checkRead == 1);
@@ -48,34 +49,19 @@ void readIn()
 	  checkRead = scanf("%1d", &matIn[j][i]);
 	}
     }
-  
+ 
+  //INIT 
+  idNextCol = 0;
   //prepare the set
   if(!Q.empty()) Q.clear();  
   REP(i, nbCols-1) Q.insert(i+1);//col 0 is fixed
 
   //init the idx of Last One
   REP(i, nbLines) idxLastOne[i] = -1;
-}
+  REP(i, nbCols) REP(j, nbLines) matOut[i][j] = -1;
 
-void search()
-{
-  if(Q.empty()) return;
-  
-  FOREACH(i, Q)
-    {
-      //get a collumn
-      int idColIn = *i;
-      if(setCol(idColIn, idNextCol)) 
-	{
-	  Q.erase(i);
-	  idNextCol++;
-	  search();
-
-	  Q.insert(idColIn);
-	  
-	}
-    }
-  
+  if(!results.empty()) results.clear();
+  REP(i, nbCols) results.push_back(0);
 }
 
 
@@ -95,11 +81,11 @@ bool setCol(int idIn, int idOut)//set the collumn idIn to the collumn idOut
     {
       matOut[idOut][i] = matIn[idIn][i];
       //update idx of Last One
+      //idxLastOnePrevious[i] = idxLastOne[i];
       if(matIn[idIn][i] == 1)
 	idxLastOne[i] = idOut;
     }
-  return true;
-  
+  return true;  
 }
 
 void printMatIn()
@@ -125,24 +111,76 @@ void printMatOut()
   printf("\n");  
 }
 
+
+void search()
+{
+  //  if(Q.empty()) throw 0;
+  
+  FOREACH(i, Q)
+    {
+      //get a collumn
+      int idColIn = *i;
+
+      //int old_idNextCol = idNextCol;
+      vi oldLastOne(nbLines, -1);
+      REP(i, nbLines) oldLastOne[i] = idxLastOne[i];
+      if(setCol(idColIn, idNextCol)) //go for this call
+	{
+	  results[idNextCol] = idColIn;
+	  if(debug)
+	    {
+	      printf("set col %d of input to col %d in output\n", idColIn, idNextCol);
+	      printMatOut();
+	    }
+	  if(Q.size() == 1) throw 0;
+	  Q.erase(i);
+	  idNextCol++;
+
+	  search();
+	  
+	  //go here i.e.
+	  Q.insert(idColIn);
+	  idNextCol--;
+	  
+	  REP(i, nbLines) idxLastOne[i] = oldLastOne[i];
+	}
+      else if(debug) printf("can not set col %d of input to col %d in output\n", idColIn, idNextCol);
+    }
+  
+}
+
+
 void compute()
 {
   setCol(0, 0);
   idNextCol++;
-  search(1);
+  try
+    {
+      search();
+    }
+  catch(int)
+    {
+      //printf("result is\n");
+      //printMatOut();
+      REP(i, nbCols) printf("%d\n", results[i]);
+    }
 }
 
 int main()
 {
-  /*int nbCases;
+  int nbCases;
   checkRead = scanf("%d", &nbCases);
   while(nbCases--)
     {
+      
       readIn();
-      printMatIn();
-      }*/
-
+      //printMatIn();
+      compute();     
+      if(nbCases) printf("\n");
+    }
+  
   //  test set  
+  /*
   Q.insert(3);
   Q.insert(4);
   Q.insert(5);
@@ -154,7 +192,7 @@ int main()
 
   FOREACH(i,Q) Q.erase(i);
 
-  FOREACH(i,Q) cout << *i << endl;
+  FOREACH(i,Q) cout << *i << endl;*/
   
   return 0;  
 }
