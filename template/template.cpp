00b50016f33d84e2048f00b8784f346bb34da7de
HongAnhVu <vuhonganh91@gmail.com>
2015-06-24 10:01:45 +0200

    update some files

--- template/template.cpp
+++ template/template.cpp
@@ -11,9 +11,14 @@
 
 using namespace std;
 
+#define INT_MAX 2147483647
+#define INT_MIN -2147483647
+#define pi acos(-1.0)
+
+
 #define REP(i, n) for(int i = 0; i < (n); ++i)
 
-#define FOR(i, a, b) for(int i = (a), i < (b); ++i)
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
 
 #define VAR(v, x) __typeof(x) v = x
 
@@ -28,9 +33,15 @@ typedef vector<ii> vii;
 
 bool debug = false;
 int checkRead;
+
+
+int ri() {int res; checkRead = scanf("%d", &res); assert(checkRead == 1); return res;}
+
+bool ri(int &intIN) {checkRead = scanf("%d", &intIN); return (checkRead != EOF);}
+
+
 //done shortcut
 ////////////////
-
  
 
 void readIn()
